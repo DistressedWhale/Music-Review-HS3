e127a1f87d02df40602f925cb83ce82ab8a41b0f
Your name <ashughes001@gmail.com>
2017-10-14 16:19:51 +0100

    adding sentence to words array funciton

---
+++ Ruby/splitStrings.rb
@@ -0,0 +1,11 @@
+#String splitter. E.g. takes sentence and cuts the words out into an Array.
+#Sentence is a String argument.
+def stringSplit(sentence)
+wordArray = []  #this array is to contain each of the words from the sentence (or paragraph, or entire music review)
+wordArray = sentence.split(/\W+/)
+puts wordArray[0] #returns the first word.
+end
+
+#Code below demonstrates the function working, for instance, on a kanye review.
+kanyeWest_Yeezus_Review = "absolute masterpiece, beautiful and at times very raw."
+puts stringSplit(kanyeWest_Yeezus_Review)
