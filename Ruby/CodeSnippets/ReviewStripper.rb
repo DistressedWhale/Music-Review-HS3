fd21c3d4fd5720e0feb3fe2a04bc0fd4e06e7ca1
SamWhale <32313989+SamWhale@users.noreply.github.com>
2017-10-14 19:19:28 +0100

    Update ReviewStripper.rb

--- Ruby/CodeSnippets/ReviewStripper.rb
+++ Ruby/CodeSnippets/ReviewStripper.rb
@@ -1,8 +1,11 @@
-require 'PearsonLongman-Dictionary'
+require 'PearsonLongman-Dictionary' #Main dictionary Gem
+
 class ReviewStripper
   include DictionaryLookup
-  def getWordType(word)
+  
+  def getWordType(word) #takes a string as input, returns the lookup of the type
     return DictionaryLookup::Base.define(word).first.part_of_speech
   end
+  
 end
 puts ReviewStripper.new.getWordType("Hello")
