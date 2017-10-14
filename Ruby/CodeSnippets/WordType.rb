e97a0d2eb5fcbf4ed592d289f74f3fd48133afda
midgetviper <alex-s2011@hotmail.co.uk>
2017-10-14 19:30:30 +0100

    Add files via upload

--- Ruby/CodeSnippets/WordType.rb
+++ Ruby/CodeSnippets/WordType.rb
@@ -1,21 +1,57 @@
+  require 'sort_alphabetical'
+  
   @Words = ['Hello', 'go', 'great', 'fabulous']
   @Types = ['noun', 'noun', 'adjective', 'adjective']
 
+  #Finds the adjectives
   def method_WordTypes
     Words.each_index do |i|
       Types[i] = 'null' if Types[i] != 'adjective'
     end
 	return y
   end
-
-  method_WordTypes(Words, Types)
   
-  
+  #Puts the adjectives in an array
   def method_List
-	List = array.new
+	list = array.new
 	method_WordTypes(Words, Types)
     Types.each_index do |i|
 		list << Types[i] if Types[i] = 'adjective'
 	end
 	return list
   end
+  
+  #orders list
+  def method_Sort(method_list)
+	list = array.new
+	list = method_List.sort_alphabetical
+    return list
+  end
+  
+  #counts frequency
+  def method_Frequency
+	list = method_Sort
+	frequency = [[]]
+	list.each_index do |i|
+	  frequency_word = []
+	  list.each_index do |i + 1|
+		if list[i] == list[i + 1]
+		  k+=1
+		else
+		  frequency_word = [list[i], k]
+		  k = 0
+	    end
+	    frequency[i] = frequency_word
+      end
+	  return frequency
+  end
+  
+  def method_Weight(method_frequency)
+    weight = [[]]
+	list = method_Frequency
+	list.each_index do |i|
+	  weight_2 = list_[2]**(-1)
+	  weight[i] = [[list[i], weight_2]
+	 end
+	return weight
+	end
