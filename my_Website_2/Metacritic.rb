2e68db667b12b70c3927a160ce01ac0dcc4d58e5
AlexxHD <32795614+AlexxHD@users.noreply.github.com>
2017-10-15 10:25:45 +0100

    Add files via upload
    this takes in a bigger sample size (also includes User reviews)
    
    warning you need a different URL for this.

--- my_Website_2/Metacritic.rb
+++ my_Website_2/Metacritic.rb
@@ -4,9 +4,20 @@ require 'rubygems'
 require 'nokogiri' #Uses gem NokoGiri which turns website into an object in rubygems
 require 'open-uri' #This gem makes an HTTP request easy for accessing websites.
 class Metacritic
-  def getWebsite(reviewURL) #method with input as the website
-    page = Nokogiri::HTML(open(reviewURL, "User-Agent" => "Whatever you want here")) #makes object
-    page.css("div.review_body").text
+  def getWebsite(baseURL) #method with input as the website
+    #reviewURL http://www.metacritic.com/music/yeezus/kanye-west/critic-reviews
+    #userURL http://www.metacritic.com/music/yeezus/kanye-west/user-reviews
+    text = ""
+    userCritic = ["user", "critic"]
+
+    userCritic.each do |person|
+      page = Nokogiri::HTML(open(baseURL + person + '-reviews', "User-Agent" => "Whatever you want here")) #makes object
+      text += page.css("div.review_body").text
+    end
+
+    return text
     #return "Great Amazing hello incredible world beautiful"
   end
 end
+
+# puts getWebsite("http://www.metacritic.com/music/yeezus/kanye-west/")
