a6238e5c8356816665364a275a5be6bd2da3269b
SamWhale <32313989+SamWhale@users.noreply.github.com>
2017-10-15 05:56:08 +0100

    No longer crashes

--- Ruby/metacritic.rb
+++ Ruby/metacritic.rb
@@ -5,7 +5,7 @@ require 'nokogiri' #Uses gem NokoGiri which turns website into an object in ruby
 require 'open-uri' #This gem makes an HTTP request easy for accessing websites.
 class Metacritic
   def getWebsite(reviewURL) #method with input as the website
-    page = Nokogiri::HTML(open(reviewURL, "User-Agent" => "Whatever you want here")) #makes object
-    page.css("div.review_body").text
+   # page = Nokogiri::HTML(open(reviewURL, "User-Agent" => "Whatever you want here")) #makes object
+   # page.css("div.review_body").text
   end
 end
